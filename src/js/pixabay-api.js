c0e37f8da10538e3780febeb00a9e8296f611a01
BohdanLysniak <marttelo@ukr.net>
2024-03-30 17:44:59 +0200

    refactor-pix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,14 @@
-export function getImage(query) {
+import axios from "axios";
+export async function getImage(query) {
   const BASE_URL = "https://pixabay.com/api/"
-  const params = new URLSearchParams({
+  const params = {
     key: "43011845-ca4f94d8d544ea7c100ad449b",
     q: query,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
-  })
+  };
 
-  const url = `${BASE_URL}?${params}`;
-
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-}
+  const res = await axios.get(BASE_URL, { params });
+  return res.data;
+};
